e117c2cda9bd86a0a26ec5ca8f97b38e6c833606
Shanti <fishfinna12@gmail.com>
2023-10-08 23:11:09 -0700

    added the runner for uploads to the docker config

--- auth/app.py
+++ auth/app.py
@@ -10,24 +10,25 @@ app.config['SECRET_KEY'] = "jwt-secret-key"
 with open("users.json") as f:
     users = json.load(f)
 
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        #get user info
+        # get user info
         username = request.form.get("username")
         password = request.form.get("password")
 
-        #hash the password
+        # hash the password
         hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
         usernames = [i["username"] for i in users]
         passes = [i["password"] for i in users]
-        
-        #check that the password and username are correct
+
+        # check that the password and username are correct
         if username in usernames and hashed_password in passes:
             token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow(
             ) + datetime.timedelta(minutes=10)}, app.config['SECRET_KEY'], algorithm='HS256')
-            response = make_response(redirect("http://localhost:8080/stream"))
+            response = make_response(redirect("http://localhost:8090"))
             response.set_cookie("token", token, httponly=True)
             return response
         else:
--- upload/app.js
+++ upload/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 8090;
 
 app.use(express.static(path.join(__dirname, "/public")));
 
